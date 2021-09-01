1301177fc73fce32eaa1da967f59910d662b605a
Jerome <josejeromelalunio@gmail.com>
2021-09-01 17:16:21 +0800

    setup consumers for UserProvider

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:memory_lamp/models/enums/views.dart';
+import 'package:memory_lamp/providers/user_provider.dart';
 import 'package:memory_lamp/providers/view_provider.dart';
 import 'package:memory_lamp/widgets/compound_widgets/ml_appbar.dart';
 import 'package:memory_lamp/widgets/compound_widgets/ml_drawer.dart';
@@ -10,8 +11,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ViewProvider(),
+    return MultiProvider(
+      providers: [
+        ListenableProvider<ViewProvider>(create: (_) => ViewProvider()),
+        ListenableProvider<UserProvider>(create: (_) => UserProvider()),
+      ],
       child: Scaffold(
         appBar: const MLAppbar(),
         drawer: const MLDrawer(),
--- lib/widgets/compound_widgets/user_stats.dart
+++ lib/widgets/compound_widgets/user_stats.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:memory_lamp/helpers/asset_manager.dart';
 import 'package:memory_lamp/helpers/size_mq.dart';
+import 'package:memory_lamp/models/user.dart';
+import 'package:memory_lamp/providers/user_provider.dart';
 import 'package:memory_lamp/theming/ml_colors.dart';
 import 'package:memory_lamp/theming/ml_font.dart';
 import 'package:memory_lamp/widgets/compound_widgets/labeled_icon.dart';
 import 'package:memory_lamp/widgets/ml_container.dart';
 import 'package:memory_lamp/widgets/ml_text.dart';
+import 'package:provider/provider.dart';
 
 class UserStats extends StatelessWidget {
-  const UserStats({Key? key}) : super(key: key);
+  final inDrawer;
+
+  const UserStats({this.inDrawer = false, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Consumer<UserProvider>(
+      builder: (BuildContext context, userProvider, Widget? child) {
+        if (inDrawer) {
+          return _inDrawer(userProvider.data);
+        }
+        return _asHeader(userProvider.data);
+      },
+    );
+  }
+
+  MLContainer _inDrawer(User _user) {
+    return MLContainer(
+      backgroundColor: MLColors.primaryLight,
+      padding: EdgeInsets.all(8),
+      child: LabeledIcon(
+        icon: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Image.asset(AssetManager.icon("pfp.png")),
+        ),
+        label: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MLText(_user.name, style: MLFont.extraLargeBold),
+            MLText("Streak : ${_user.streak}"),
+            MLText("Week: ${_user.week}"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  MLContainer _asHeader(User _user) {
     return MLContainer(
       padding: const EdgeInsets.all(20.0),
       borderRadius: BorderRadius.only(
@@ -19,40 +56,38 @@ class UserStats extends StatelessWidget {
         bottomRight: Radius.circular(28),
       ),
       backgroundColor: MLColors.primary,
-      child: Center(
-        child: LabeledIcon(
-          spaceBetween: 24,
-          icon: Image.asset(AssetManager.icon("pfp.png")),
-          label: SizedBox(
-            width: SizeMQ.width! * .4,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MLText(
-                  "Hannah",
-                  fontColor: Colors.white,
-                  fontWeight: MLFont.bold,
-                  fontSize: MLFont.extraLarge,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    MLText(
-                      "Streak : 1",
-                      fontColor: Colors.white,
-                      fontSize: MLFont.mediumSmall,
-                      fontWeight: MLFont.semiBold,
-                    ),
-                    MLText(
-                      "Week: 0",
-                      fontColor: Colors.white,
-                      fontSize: MLFont.mediumSmall,
-                      fontWeight: MLFont.semiBold,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+      child: LabeledIcon(
+        spaceBetween: 24,
+        icon: Image.asset(AssetManager.icon("pfp.png")),
+        label: SizedBox(
+          width: SizeMQ.width! * .4,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MLText(
+                _user.name,
+                fontColor: Colors.white,
+                fontWeight: MLFont.bold,
+                fontSize: MLFont.extraLarge,
+              ),
+              Row(
+                children: [
+                  MLText(
+                    "Streak : ${_user.streak}",
+                    fontColor: Colors.white,
+                    fontSize: MLFont.mediumSmall,
+                    fontWeight: MLFont.semiBold,
+                  ),
+                  Spacer(),
+                  MLText(
+                    "Week: ${_user.week}",
+                    fontColor: Colors.white,
+                    fontSize: MLFont.mediumSmall,
+                    fontWeight: MLFont.semiBold,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
