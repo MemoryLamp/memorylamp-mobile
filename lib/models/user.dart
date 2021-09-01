35bc8ece35a1b9a57c4f4b9ed03b52724aaf8c35
Jerome <josejeromelalunio@gmail.com>
2021-09-01 15:33:51 +0800

    add user model and provider

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class User {
+  String name;
+  int streak;
+  int week;
+
+  User({
+    required this.name,
+    required this.streak,
+    required this.week,
+  });
+}
---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:memory_lamp/models/user.dart';
+
+class UserProvider extends ChangeNotifier {
+  User _user = User(name: "Hannah", streak: 1, week: 0);
+
+  void updateUserData({String? name, int? streak, int? week}) {
+    if (name != null) _user.name = name;
+    if (streak != null) _user.streak = streak;
+    if (week != null) _user.week = week;
+
+    notifyListeners();
+  }
+
+  User get data => _user;
+}
