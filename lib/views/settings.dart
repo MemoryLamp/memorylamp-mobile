3942aa2c9cb14485c645af5710352e1ef8574aa1
Jerome <josejeromelalunio@gmail.com>
2021-09-02 11:45:04 +0800

    add ToggleButtons

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:memory_lamp/helpers/size_mq.dart';
 import 'package:memory_lamp/theming/defaults.dart';
+import 'package:memory_lamp/theming/ml_colors.dart';
 import 'package:memory_lamp/theming/ml_font.dart';
 import 'package:memory_lamp/widgets/ml_container.dart';
 import 'package:memory_lamp/widgets/ml_text.dart';
@@ -21,12 +23,57 @@ class SettingsView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              MLText(
-                "Verses Notification",
-                style: MLFont.bannerText01,
-              ),
+              _banner(),
+              _toggleButtons(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  MLText _banner() {
+    return MLText(
+      "Verses Notification",
+      style: MLFont.bannerText01,
+    );
+  }
+
+  Padding _toggleButtons() {
+    MLText _text(String text) {
+      return MLText(
+        text,
+        fontWeight: MLFont.semiBold,
+        fontSize: MLFont.medium,
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Center(
+        child: ToggleButtons(
+          borderRadius: BorderRadius.circular(24),
+          fillColor: MLColors.primary,
+          selectedColor: Colors.white,
+          selectedBorderColor: MLColors.primary,
+          borderColor: MLColors.primary,
+          constraints: BoxConstraints.expand(
+            width: SizeMQ.width! * .25,
+            height: MLFont.extraLarge,
+          ),
+          children: [
+            _text("Daily"),
+            _text("Weekly"),
+            _text("Monthly"),
+          ],
+          isSelected: [
+            false,
+            true,
+            false,
+          ],
+          onPressed: (index) {
+            print(index);
+          },
         ),
       ),
     );
