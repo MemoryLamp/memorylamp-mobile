be14fed787bf5b6cdd6938d0de9e2183732cd1b1
Jerome <josejeromelalunio@gmail.com>
2022-02-22 10:32:51 +0800

    add image_label_pair

---
+++ lib/models/image_label_pair.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ImageLabelPair {
+  final String name;
+  final Image image;
+
+  const ImageLabelPair({required this.name, required this.image});
+}
